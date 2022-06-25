3474da0a3869566b26a1d9b878bc8e2825748a47
Joel Júnior <junior.tads@gmail.com>
2022-06-25 16:23:24 -0300

    Update readsensor.go

--- readsensor.go
+++ readsensor.go
@@ -50,23 +50,5 @@ func main() {
 		lg.Fatal(err)
 	}
 
-	//fmt.Printf("Sensor = %v: Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-	//	sensorType, temperature, humidity, retried)
-	// calculate vpd
-	// J. Win. (https://physics.stackexchange.com/users/1680/j-win),
-	// How can I calculate Vapor Pressure Deficit from Temperature and Relative Humidity?,
-	// URL (version: 2011-02-03): https://physics.stackexchange.com/q/4553
-	temperature64 := float64(temperature)
-	humidity64 := float64(humidity)
-
-	es := 0.6108 * math.Exp(17.27*temperature64/(temperature64+237.3))
-	ea := humidity64 / 100 * es
-
-	// this equation returns a negative value (in kPa), which while technically correct,
-	// is invalid in this case because we are talking about a deficit.
-	vpd := (ea - es) * -1
-
-	str := fmt.Sprintf("%s temperature=%f,humidity=%f,vpd=%f,retried=%d %d",
-		stype, temperature, humidity, vpd, retried, time.Now().UnixNano())
-	fmt.Fprintln(os.Stdout, str)
+	fmt.Printf("Sensor = %v: Temperature = %v*C, Humidity = %v%% (retried %d times)\n", sensorType, temperature, humidity, retried)
 }
