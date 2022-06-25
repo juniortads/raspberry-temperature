588059eefd30ad38276f01da04b798d552150ef9
Joel Júnior <junior.tads@gmail.com>
2022-06-25 15:41:43 -0300

    Create readsensor.go

---
+++ readsensor.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MichaelS11/go-dht"
+)
+
+func main() {
+	err := dht.HostInit()
+	if err != nil {
+		fmt.Println("HostInit error:", err)
+		return
+	}
+
+	dht, err := dht.NewDHT("GPIO19", dht.Fahrenheit, "")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
+		return
+	}
+
+	humidity, temperature, err := dht.Read()
+	if err != nil {
+		fmt.Println("Read error:", err)
+		return
+	}
+	fmt.Printf("humidity: %v\n", humidity)
+	fmt.Printf("temperature: %v\n", temperature)
+}
